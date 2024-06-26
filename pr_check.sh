558c4cf9fd5f6eb00009f66731d25afe945f6bc1
Mike DePaulo <mikedep333@users.noreply.github.com>
2024-06-26 10:42:26 -0400

    Revert cleaning up the Jenkins pr_check.sh (#152)
    Until Konflux can do tests successfully
    
    PULP-110

--- pr_check.sh
+++ pr_check.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# Being migrated to
-# https://github.com/redhat-appstudio/tenants-config/pull/425
+APP_NAME="pulp"       # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="pulp" # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/pulp-ubi"
 
-# Haven't been able to specify this yet
+# be explicit about what to build
+DOCKERFILE=Dockerfile
+
+IQE_PLUGINS="content-sources"
+IQE_MARKER_EXPRESSION="api"
+IQE_FILTER_EXPRESSION="not test_introspection_of_persistent_user"
+IQE_CJI_TIMEOUT="30m"
+COMPONENTS_W_RESOURCES="pulp"
 REF_ENV="insights-stage"
 
-# We disabled this, not sure how to disable it there.
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/cicd-tools/main
+curl -s $CICD_URL/bootstrap.sh >.cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# Include all impacted HMS apps for deployment
+EXTRA_DEPLOY_ARGS="--set-parameter pulp/PULP_API_MEMORY_LIMIT=4096Mi --set-parameter pulp/PULP_API_MEMORY_REQUEST=2048Mi provisioning sources image-builder-crc"
+
+source $CICD_ROOT/build.sh
+source $CICD_ROOT/deploy_ephemeral_env.sh
+source $APP_ROOT/unit_test.sh
 # source $CICD_ROOT/cji_smoke_test.sh
+source $CICD_ROOT/post_test_results.sh
