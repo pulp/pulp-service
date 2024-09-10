9794692defca4b9e81a7ade96f676cb4ad4d7087
Dennis Kliban <dkliban@redhat.com>
2024-09-10 15:54:14 -0400

    Switch domain field in DomainOrg model to OneToOneField (#244)

--- pulp_service/pulp_service/app/models.py
+++ pulp_service/pulp_service/app/models.py
@@ -9,7 +9,7 @@ class DomainOrg(models.Model):
     One-to-many relationship between org ids and Domains.
     """
     org_id = models.CharField(null=True, db_index=True)
-    domain = models.ForeignKey(Domain, related_name="domains", on_delete=models.CASCADE, unique=True)
+    domain = models.OneToOneField(Domain, related_name="domains", on_delete=models.CASCADE)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name="users", on_delete=models.CASCADE, null=True
     )
