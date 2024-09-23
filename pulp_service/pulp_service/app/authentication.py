9fae34221ac5378c1a6bdb92d8fd809930906795
André "decko" de Brito <decko@redhat.com>
2024-09-23 10:48:36 -0300

    Add the authenticate_header method to our own auth class

--- pulp_service/pulp_service/app/authentication.py
+++ pulp_service/pulp_service/app/authentication.py
@@ -5,3 +5,6 @@ class RHServiceAccountCertAuthentication(JSONHeaderRemoteAuthentication):
 
     header = "HTTP_X_RH_IDENTITY"
     jq_filter = ".identity.x509.subject_dn"
+
+    def authenticate_header(self, request):
+        return "Bearer"
