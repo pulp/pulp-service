f070441e7991edf95312ffc70b8d33875257321e
André "decko" de Brito <decko@redhat.com>
2024-11-05 11:50:38 -0300

    Add a new secret for ephemeral and other changes

--- pulp_service/pulp_service/app/settings.py
+++ pulp_service/pulp_service/app/settings.py
@@ -6,5 +6,5 @@ Check `Plugin Writer's Guide`_ for more details.
 """
 
 RH_IDENTITY_HEADER = "HTTP_X_RH_IDENTITY"
-SUBSCRIPTION_API_URL = "https://subscription.stage.api.redhat.com/svcrest/subscription/v5/featureStatus?accountId=5894300&features=RHEL-OS-x86_64&features=RHEL-EUS-x86_64"
+SUBSCRIPTION_API_URL = "https://subscription.stage.api.redhat.com/svcrest/subscription/v5/featureStatus"
 SUBSCRIPTION_CERT_PATH = "/var/certs/pulp_nonprod_cert.pem"
