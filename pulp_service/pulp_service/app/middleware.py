5398ed94f3ca562c1926cb79904ca75e3c52700f
Dennis Kliban <dkliban@redhat.com>
2024-11-08 11:07:19 -0500

    Revert "Adds OCIStorage backend and middleware for OCIStorage backend. (#334)"
    This reverts commit 9a2ae4ea3d5ca12de2195aa316a691f2a1b5814d.

--- pulp_service/pulp_service/app/middleware.py
+++ pulp_service/pulp_service/app/middleware.py
@@ -1,5 +1,4 @@
 from contextlib import suppress
-from contextvars import ContextVar
 import cProfile
 import logging
 import marshal
@@ -9,12 +8,10 @@ from django.db import IntegrityError
 from django.utils.deprecation import MiddlewareMixin
 
 from pulpcore.app.models import Artifact
-from pulpcore.app.util import get_artifact_url, get_worker_name, resolve_prn
+from pulpcore.app.util import get_artifact_url, get_worker_name
 
 
 _logger = logging.getLogger(__name__)
-repository_name_var = ContextVar('repository_name')
-x_quay_auth_var = ContextVar('x_quay_auth')
 
 
 class ProfilerMiddleware(MiddlewareMixin):
@@ -60,14 +57,3 @@ class ProfilerMiddleware(MiddlewareMixin):
                 _logger.info(f"Profile data URL: {get_artifact_url(artifact)}")
 
         return response
-
-
-class OCIStorageMiddleware(MiddlewareMixin):
-    def process_view(self, request, callback, callback_args, callback_kwargs):
-        # Set the repository name in a contextvar
-        if 'repository' in request.POST:
-            m, pk = resolve_prn(request.POST['repository'])
-            repository_name = m.objects.get(pk=pk).name
-            repository_name_var.set(repository_name)
-        if 'HTTP_X_QUAY_AUTH' in request.META:
-            x_quay_auth_var.set(request.META['HTTP_X_QUAY_AUTH'])
