e027f977a07449ad36ff13b6051a6729d530787b
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2024-11-18 16:04:42 -0300

    Modify check uris to match configured route paths

--- pulp_service/pulp_service/app/urls.py
+++ pulp_service/pulp_service/app/urls.py
@@ -6,7 +6,7 @@ from .viewsets import RedirectCheck, InternalServerErrorCheck, InternalServerErr
 
 urlpatterns = [
     path("api/pulp-admin/", admin_site.urls),
-    path("api/redirect-check/", RedirectCheck.as_view()),
-    path("api/internal-server-error-check/", InternalServerErrorCheck.as_view()),
-    path("api/raise-exception-check/", InternalServerErrorCheckWithException.as_view()),
+    path("api/pulp/redirect-check/", RedirectCheck.as_view()),
+    path("api/pulp/internal-server-error-check/", InternalServerErrorCheck.as_view()),
+    path("api/pulp/raise-exception-check/", InternalServerErrorCheckWithException.as_view()),
 ]
