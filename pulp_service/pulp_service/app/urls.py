7dd770d2ac8dce20328438fb56ca12de5633b8aa
Humberto Yagi <45576767+git-hyagi@users.noreply.github.com>
2024-11-18 13:12:59 -0300

    Add the redirect and internalerror check endpoints (#345)

--- pulp_service/pulp_service/app/urls.py
+++ pulp_service/pulp_service/app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
 from .admin import admin_site
+from .viewsets import RedirectCheck, InternalServerErrorCheck, InternalServerErrorCheckWithException
+
 
 urlpatterns = [
     path("api/pulp-admin/", admin_site.urls),
+    path("api/redirect-check/", RedirectCheck.as_view()),
+    path("api/internal-server-error-check/", InternalServerErrorCheck.as_view()),
+    path("api/raise-exception-check/", InternalServerErrorCheckWithException.as_view()),
 ]
---
+++ pulp_service/pulp_service/app/viewsets.py
@@ -0,0 +1,56 @@
+from django.shortcuts import redirect
+from rest_framework import status
+from rest_framework.exceptions import APIException
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class RedirectCheck(APIView):
+    """
+    Handles requests to the /api/redirect-check/ endpoint.
+    """
+
+    # allow anyone to access the endpoint
+    authentication_classes = []
+    permission_classes = []
+
+    def head(self, request=None, path=None, pk=None):
+        """
+        Responds to HEAD requests for the redirect-check endpoint.
+        """
+        return redirect("/api/")
+
+
+# returning 500 error in a "graceful" way
+class InternalServerErrorCheck(APIView):
+    """
+    Handles requests to the /api/internal-server-error-check/ endpoint.
+    """
+
+    # allow anyone to access the endpoint
+    authentication_classes = []
+    permission_classes = []
+
+    def head(self, request=None, path=None, pk=None):
+        """
+        Responds to HEAD requests for the internal-server-error-check endpoint.
+        """
+        return Response(data=None, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# raising an exception (helpful to verify middleware's behavior, for example, otel)
+class InternalServerErrorCheckWithException(APIView):
+    """
+    Handles requests to the /api/raise-exception-check/ endpoint.
+    """
+
+    # allow anyone to access the endpoint
+    authentication_classes = []
+    permission_classes = []
+
+    def head(self, request=None, path=None, pk=None):
+        """
+        Responds to HEAD requests for the raise-exception-check endpoint.
+        """
+        # the drf APIException returns a HTTP_500_INTERNAL_SERVER_ERROR
+        raise APIException()
