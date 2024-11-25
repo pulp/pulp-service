3a9dbdd8f57045e4f57bf1cf8a0d5ece2c274341
André "decko" de Brito <decko@redhat.com>
2024-11-25 15:43:52 -0300

    Set AUTHENTICATION_HEADER_DEBUG to False (#356)

--- pulp_service/pulp_service/app/settings.py
+++ pulp_service/pulp_service/app/settings.py
@@ -7,4 +7,4 @@ Check `Plugin Writer's Guide`_ for more details.
 
 SUBSCRIPTION_API_URL = "https://subscription.stage.api.redhat.com/svcrest/subscription/v5/featureStatus"
 SUBSCRIPTION_API_CERT = ""
-AUTHENTICATION_HEADER_DEBUG = True
+AUTHENTICATION_HEADER_DEBUG = False
