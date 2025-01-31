fba8525ffe5cd43a9a0304a5b666362f87f7235a
André "decko" de Brito <decko@redhat.com>
2025-01-31 15:43:00 -0300

    Add a AdminFilter to filter content-source domains (#393)

--- pulp_service/pulp_service/app/admin.py
+++ pulp_service/pulp_service/app/admin.py
@@ -11,6 +11,28 @@ class PulpAdminSite(admin.AdminSite):
     site_header = "Pulp administration"
 
 
+class ContentSourceDomainFilter(admin.SimpleListFilter):
+    title = 'ContentSource Domains'
+    parameter_name = 'content_source_filter'
+
+    def lookups(self, request, model_admin):
+        return [
+            ("cs-domains", "Content Source Domains"),
+            ("non-cs-domains", "Non Content Source Domains"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "cs-domains":
+            return queryset.filter(
+                description__contains="content-sources"
+            )
+        if self.value() == "non-cs-domains":
+            return queryset.exclude(
+                description__contains="content-sources"
+            )
+        return queryset
+
+
 class DomainOrgAdmin(admin.ModelAdmin):
     list_display = ["domain", "user", "org_id"]
     list_editable = ["user", "org_id"]
@@ -19,7 +41,7 @@ class DomainOrgAdmin(admin.ModelAdmin):
 
 class DomainAdmin(admin.ModelAdmin):
     list_display = ["name", "description", "storage_class"]
-    list_filter = ["name", "description", "storage_class"]
+    list_filter = ["name", "description", "storage_class", ContentSourceDomainFilter]
 
 
 admin_site = PulpAdminSite(name="myadmin")
