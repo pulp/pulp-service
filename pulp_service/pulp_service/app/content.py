9a4537b24a5aa48007b17c793e623ef498d768f0
Humberto Yagi <45576767+git-hyagi@users.noreply.github.com>
2025-02-18 17:04:57 -0300

    Log pulp-content request headers (#405)

--- pulp_service/pulp_service/app/content.py
+++ pulp_service/pulp_service/app/content.py
@@ -1,10 +1,13 @@
 import json
+import logging
+
 from aiohttp import web
 from base64 import b64decode
 from frozenlist import FrozenList
 
 from pulpcore.plugin.content import app
 
+_logger = logging.getLogger(__name__)
 
 @web.middleware
 async def add_rh_org_id_resp_header(request, handler):
@@ -12,6 +15,8 @@ async def add_rh_org_id_resp_header(request, handler):
         response = await handler(request)
     except web.HTTPException as exc:
         return exc
+    
+    _logger.info(f"REQUEST HEADERS: {request.headers}")
 
     if not request.headers.get("HTTP_X_RH_IDENTITY"):
         return response
