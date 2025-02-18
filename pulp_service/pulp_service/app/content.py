60f39e955d4d38524c025ebda887bd70b75884d6
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2025-02-18 17:15:11 -0300

    Fix the rh-identity header

--- pulp_service/pulp_service/app/content.py
+++ pulp_service/pulp_service/app/content.py
@@ -1,13 +1,10 @@
 import json
-import logging
-
 from aiohttp import web
 from base64 import b64decode
 from frozenlist import FrozenList
 
 from pulpcore.plugin.content import app
 
-_logger = logging.getLogger(__name__)
 
 @web.middleware
 async def add_rh_org_id_resp_header(request, handler):
@@ -16,12 +13,10 @@ async def add_rh_org_id_resp_header(request, handler):
     except web.HTTPException as exc:
         return exc
     
-    _logger.info(f"REQUEST HEADERS: {request.headers}")
-
-    if not request.headers.get("HTTP_X_RH_IDENTITY"):
+    if not request.headers.get("x-rh-identity"):
         return response
 
-    rh_identity_header = request.headers["HTTP_X_RH_IDENTITY"]
+    rh_identity_header = request.headers["x-rh-identity"]
     rh_identity_header_decoded = b64decode(rh_identity_header)
     rh_identity_header_json = json.loads(rh_identity_header_decoded)
 
