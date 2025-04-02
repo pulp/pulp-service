989a9fd4cd8379eee42054030961db3895a2d8b8
Humberto Yagi <45576767+git-hyagi@users.noreply.github.com>
2025-04-02 07:37:55 -0300

    Add functional tests for scan functionality (#475)

---
+++ pulp_service/pulp_service/tests/functional/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pulpcore.tests.functional.utils import BindingsNamespace
+
+# Bindings API Fixtures
+
+
+@pytest.fixture(scope="session")
+def service_bindings(_api_client_set, bindings_cfg):
+    """
+    A namespace providing preconfigured pulp_service api clients.
+    """
+    from pulpcore.client import pulp_service as service_bindings_module
+
+    api_client = service_bindings_module.ApiClient(bindings_cfg)
+    _api_client_set.add(api_client)
+    yield BindingsNamespace(service_bindings_module, api_client)
+    _api_client_set.remove(api_client)
+
+
+@pytest.fixture(scope="session")
+def vuln_report_api(service_bindings):
+    """Vulnerability Report API fixture."""
+    return service_bindings.VulnReportApi
