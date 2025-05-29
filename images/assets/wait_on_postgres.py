9a983415f0d7437b3c7cc701f5be85ee2ff16f21
André "decko" de Brito <decko@redhat.com>
2025-05-29 17:59:36 -0300

    Add some logging statements on wait_for_postgres script (#527)

--- images/assets/wait_on_postgres.py
+++ images/assets/wait_on_postgres.py
@@ -2,21 +2,29 @@
 
 import sys
 import time
+import logging
 from django.db import connection, utils
+
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
 
+    logging.basicConfig(level=logging.INFO)
     print("Waiting on postgresql to start...")
+    logger.info("Waiting on postgresql to start...")
     for dummy in range(100):
         try:
             connection.ensure_connection()
             break
-        except utils.OperationalError:
+        except utils.OperationalError as exp:
+            logger.warning("Connection failed. Trying again...", exc_info=True)
             time.sleep(3)
 
     else:
+        logger.error("Unable to reach postgres.")
         print("Unable to reach postgres.")
         sys.exit(1)
 
+    logger.info("Postgres started.")
     print("Postgres started.")
     sys.exit(0)
