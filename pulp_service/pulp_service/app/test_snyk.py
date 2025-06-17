790f8d8674968a00b7f452bbbc59ce6c45e1dc88
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2025-06-17 10:23:58 -0300

    test add new commit

--- pulp_service/pulp_service/app/test_snyk.py
+++ pulp_service/pulp_service/app/test_snyk.py
@@ -2,3 +2,7 @@ import os
 user_input = input("test: ")
 command = f"echo Printing {user_input}"
 os.system(command)
+
+user_input_new = input("test2: ")
+command2 = f"echo Printing {user_input_new}"
+os.system(command2)
