3d83bf7f8dbd11dc5083088a78edc91da7a4b131
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2025-06-23 15:24:45 -0300

    Reapply "Migrate from SubscriptionService to FeatureService (#554)"
    This reverts commit a2121ee58bc04796269cc3e73b82832d94c2c34f.

--- pulp_service/pulp_service/app/settings.py
+++ pulp_service/pulp_service/app/settings.py
@@ -5,8 +5,8 @@ Check `Plugin Writer's Guide`_ for more details.
     https://docs.pulpproject.org/pulpcore/plugins/plugin-writer/index.html
 """
 
-SUBSCRIPTION_API_URL = "https://subscription.stage.api.redhat.com/svcrest/subscription/v5/featureStatus"
-SUBSCRIPTION_API_CERT = ""
+FEATURE_SERVICE_API_URL = "https://feature.stage.api.redhat.com/features/v1/featureStatus"
+FEATURE_SERVICE_API_CERT = ""
 AUTHENTICATION_HEADER_DEBUG = False
 INSTALLED_APPS = "@merge django.contrib.admin.apps.SimpleAdminConfig"
 TEST_TASK_INGESTION = False
