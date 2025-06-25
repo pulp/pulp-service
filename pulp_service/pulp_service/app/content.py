04b959ed80c6e3f16baf70c5814e9e4c71282ba0
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2025-06-25 11:28:58 -0300

    Revert "Add a check for a Response type object (#558)"
    This reverts commit fd9ba2e171279edbec4a4337ec5da339c8e04362.

--- pulp_service/pulp_service/app/content.py
+++ pulp_service/pulp_service/app/content.py
@@ -21,7 +21,7 @@ async def add_rh_org_id_resp_header(request, handler):
     rh_identity_header_json = json.loads(rh_identity_header_decoded)
 
     # we need to check if the entire path exists because non-entitlement certs have a diff structure
-    if isinstance(response, web.Response) and "identity" in rh_identity_header_json and "org_id" in rh_identity_header_json["identity"]:
+    if "identity" in rh_identity_header_json and "org_id" in rh_identity_header_json["identity"]:
         response.headers["X-RH-ORG-ID"] = rh_identity_header_json["identity"]["org_id"]
 
     return response
