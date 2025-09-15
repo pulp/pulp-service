fc497163aaedc4adc3eae75689905b2b756c84d0
André "decko" de Brito <decko@redhat.com>
2025-09-15 12:50:31 -0300

    Set the `on_delete` action of the user and group to SET_NULL.

--- pulp_service/pulp_service/app/migrations/0013_domainorg_group.py
+++ pulp_service/pulp_service/app/migrations/0013_domainorg_group.py
@@ -17,11 +17,11 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='domainorg',
             name='group',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='domainorgs', to='core.group'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='domainorgs', to='core.group'),
         ),
         migrations.AlterField(
             model_name='domainorg',
             name='user',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='users', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, related_name='users', to=settings.AUTH_USER_MODEL),
         ),
     ]
